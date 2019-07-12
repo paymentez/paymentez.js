7f0f9694c43a36f190ec862e887c02205992b713
hangaritaPaymentez <hangarita@paymentez.com>
2019-07-12 16:59:27 -0500

    Cambios en timestamp

--- src/js/paymentez.js
+++ src/js/paymentez.js
@@ -1,14 +1,14 @@
 Paymentez.prototype.constructor = Paymentez;
 
-Paymentez.ENV_MODE = '';
+Paymentez.ENV_MODE = "";
 Paymentez.TEST_MODE = true;
-Paymentez.PAYMENTEZ_CLIENT_APP_CODE = '';
-Paymentez.PAYMENTEZ_CLIENT_APP_KEY = '';
+Paymentez.PAYMENTEZ_CLIENT_APP_CODE = "";
+Paymentez.PAYMENTEZ_CLIENT_APP_KEY = "";
 
 Paymentez.MERCHANT_ID = 500005;
-Paymentez.KOUNT_ENVIRONMENT = '';
-Paymentez.KOUN_TEST_ENVIRONMENT = 'https://tst.kaptcha.com/';
-Paymentez.KOUN_PROD_ENVIRONMENT = 'https://ssl.kaptcha.com/';
+Paymentez.KOUNT_ENVIRONMENT = "";
+Paymentez.KOUN_TEST_ENVIRONMENT = "https://tst.kaptcha.com/";
+Paymentez.KOUN_PROD_ENVIRONMENT = "https://ssl.kaptcha.com/";
 
 Paymentez.SERVER_LOCAL_URL = "http://localhost:8000";
 Paymentez.SERVER_DEV_URL = "https://ccapi-dev.paymentez.com";
@@ -16,56 +16,73 @@ Paymentez.SERVER_STG_URL = "https://ccapi-stg.paymentez.com";
 Paymentez.SERVER_QA_URL = "https://ccapi-qa.paymentez.com";
 Paymentez.SERVER_PROD_URL = "https://ccapi.paymentez.com";
 
-function Paymentez() {
+Paymentez.TIME_STAMP_SERVER =
+  "https://cors-anywhere.herokuapp.com/" + "http://worldtimeapi.org/api/ip";
+Paymentez.auth_timestamp = "" + String(new Date().getTime());
 
-}
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    var response = JSON.parse(xhr.responseText);
+    Paymentez.auth_timestamp = String(response.unixtime);
+    console.log("success!", Paymentez.auth_timestamp);
+  }
+};
 
-Paymentez.uuidv4 = function () {
-  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+xhr.open('GET', Paymentez.TIME_STAMP_SERVER);
+xhr.send();
+
+function Paymentez() {}
+
+Paymentez.uuidv4 = function() {
+  return "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    let r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 };
 
-Paymentez.getSessionId = function () {
+Paymentez.getSessionId = function() {
   return Paymentez.uuidv4();
 };
 
-Paymentez.getUniqToken = function (auth_timestamp, paymentez_client_app_key) {
+Paymentez.getUniqToken = function(auth_timestamp, paymentez_client_app_key) {
   let uniq_token_string = paymentez_client_app_key + auth_timestamp;
   return Paymentez.getHash(uniq_token_string);
 };
 
-Paymentez.getAuthToken = function (paymentez_client_app_code, app_client_key) {
-  let d = new Date();
-  let n = d.getTime();
-  let auth_timestamp = "" + n;
-  let string_auth_token = paymentez_client_app_code + ";" + auth_timestamp + ";" + Paymentez.getUniqToken(auth_timestamp, app_client_key);
+Paymentez.getAuthToken = function(paymentez_client_app_code, app_client_key) {
+  let string_auth_token =
+    paymentez_client_app_code +
+    ";" +
+    Paymentez.auth_timestamp +
+    ";" +
+    Paymentez.getUniqToken(auth_timestamp, app_client_key);
   return btoa(string_auth_token);
 };
 
-Paymentez.getHash = function (message) {
-  let sha256 = new jsSHA('SHA-256', 'TEXT');
+Paymentez.getHash = function(message) {
+  let sha256 = new jsSHA("SHA-256", "TEXT");
   sha256.update(message);
   return sha256.getHash("HEX");
 };
 
-Paymentez.getServerURL = function () {
+Paymentez.getServerURL = function() {
   let SERVER_URL = Paymentez.SERVER_STG_URL;
-  if (Paymentez.ENV_MODE === '') {
+  if (Paymentez.ENV_MODE === "") {
     if (Paymentez.TEST_MODE) {
       SERVER_URL = Paymentez.SERVER_STG_URL;
     } else {
       SERVER_URL = Paymentez.SERVER_PROD_URL;
     }
   } else {
-    if (Paymentez.ENV_MODE === 'dev') {
+    if (Paymentez.ENV_MODE === "dev") {
       SERVER_URL = Paymentez.SERVER_DEV_URL;
-    } else if (Paymentez.ENV_MODE === 'stg') {
+    } else if (Paymentez.ENV_MODE === "stg") {
       SERVER_URL = Paymentez.SERVER_STG_URL;
-    } else if (Paymentez.ENV_MODE === 'prod') {
+    } else if (Paymentez.ENV_MODE === "prod") {
       SERVER_URL = Paymentez.SERVER_PROD_URL;
-    } else if (Paymentez.ENV_MODE === 'prod-qa') {
+    } else if (Paymentez.ENV_MODE === "prod-qa") {
       SERVER_URL = Paymentez.SERVER_QA_URL;
     } else {
       SERVER_URL = Paymentez.SERVER_LOCAL_URL;
@@ -74,16 +91,26 @@ Paymentez.getServerURL = function () {
   return SERVER_URL;
 };
 
-Paymentez.createToken = function (createTokenRequest, successCallback, erroCallback) {
+Paymentez.createToken = function(
+  createTokenRequest,
+  successCallback,
+  erroCallback
+) {
   let SERVER_URL = this.getServerURL();
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.open("POST", SERVER_URL + "/v2/card/add", true);
-  xmlhttp.setRequestHeader("Content-Type", 'application/json');
-  xmlhttp.setRequestHeader("Auth-Token", Paymentez.getAuthToken(Paymentez.PAYMENTEZ_CLIENT_APP_CODE, Paymentez.PAYMENTEZ_CLIENT_APP_KEY));
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.setRequestHeader(
+    "Auth-Token",
+    Paymentez.getAuthToken(
+      Paymentez.PAYMENTEZ_CLIENT_APP_CODE,
+      Paymentez.PAYMENTEZ_CLIENT_APP_KEY
+    )
+  );
 
-
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState === XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+      // XMLHttpRequest.DONE == 4
       try {
         let objResponse = JSON.parse(xmlhttp.responseText);
         if (xmlhttp.status === 200) {
@@ -95,10 +122,10 @@ Paymentez.createToken = function (createTokenRequest, successCallback, erroCallb
         }
       } catch (e) {
         let server_error = {
-          "error": {
-            "type": "Server Error",
-            "help": "Server Error",
-            "description": "Server Error"
+          error: {
+            type: "Server Error",
+            help: "Server Error",
+            description: "Server Error"
           }
         };
         erroCallback(server_error);
@@ -108,38 +135,55 @@ Paymentez.createToken = function (createTokenRequest, successCallback, erroCallb
   xmlhttp.send(JSON.stringify(createTokenRequest));
 };
 
-Paymentez.dataCollector = function (session_id) {
-  if (Paymentez.ENV_MODE === '') {
+Paymentez.dataCollector = function(session_id) {
+  if (Paymentez.ENV_MODE === "") {
     if (Paymentez.TEST_MODE) {
       Paymentez.KOUNT_ENVIRONMENT = Paymentez.KOUN_TEST_ENVIRONMENT;
-
     } else {
       Paymentez.KOUNT_ENVIRONMENT = Paymentez.KOUN_PROD_ENVIRONMENT;
     }
   } else {
-    if (Paymentez.ENV_MODE === 'prod') {
+    if (Paymentez.ENV_MODE === "prod") {
       Paymentez.KOUNT_ENVIRONMENT = Paymentez.KOUN_PROD_ENVIRONMENT;
-
     } else {
       Paymentez.KOUNT_ENVIRONMENT = Paymentez.KOUN_TEST_ENVIRONMENT;
     }
   }
 
   let body, e, iframe, image;
-  if (typeof document !== 'undefined' && typeof document.body !== 'undefined' && document.body && (document.readyState === 'interactive' || document.readyState === 'complete')) {
-
-    body = document.getElementsByTagName('body')[0];
-    iframe = document.createElement('iframe');
+  if (
+    typeof document !== "undefined" &&
+    typeof document.body !== "undefined" &&
+    document.body &&
+    (document.readyState === "interactive" ||
+      document.readyState === "complete")
+  ) {
+    body = document.getElementsByTagName("body")[0];
+    iframe = document.createElement("iframe");
     iframe.setAttribute("id", "riskIframe");
     iframe.setAttribute("height", "1");
     iframe.setAttribute("scrolling", "no");
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("width", "1");
-    iframe.setAttribute("src", Paymentez.KOUNT_ENVIRONMENT + "logo.htm?m=" + Paymentez.MERCHANT_ID + "&s=" + session_id);
-    image = document.createElement('img');
+    iframe.setAttribute(
+      "src",
+      Paymentez.KOUNT_ENVIRONMENT +
+        "logo.htm?m=" +
+        Paymentez.MERCHANT_ID +
+        "&s=" +
+        session_id
+    );
+    image = document.createElement("img");
     image.setAttribute("height", "1");
     image.setAttribute("width", "1");
-    image.setAttribute("src", Paymentez.KOUNT_ENVIRONMENT + "logo.gif?m=" + Paymentez.MERCHANT_ID + "&s=" + session_id);
+    image.setAttribute(
+      "src",
+      Paymentez.KOUNT_ENVIRONMENT +
+        "logo.gif?m=" +
+        Paymentez.MERCHANT_ID +
+        "&s=" +
+        session_id
+    );
     try {
       iframe.appendChild(image);
     } catch (_error) {
@@ -149,7 +193,6 @@ Paymentez.dataCollector = function (session_id) {
   } else {
     setTimeout(Paymentez.dataCollector, 150, session_id);
   }
-
 };
 
 /**
@@ -159,7 +202,11 @@ Paymentez.dataCollector = function (session_id) {
  * @param paymentez_client_app_code provided by Paymentez.
  * @param paymentez_client_app_key provided by Paymentez.
  */
-Paymentez.setEnvironment = function (test_mode, paymentez_client_app_code, paymentez_client_app_key) {
+Paymentez.setEnvironment = function(
+  test_mode,
+  paymentez_client_app_code,
+  paymentez_client_app_key
+) {
   Paymentez.TEST_MODE = test_mode;
   Paymentez.PAYMENTEZ_CLIENT_APP_CODE = paymentez_client_app_code;
   Paymentez.PAYMENTEZ_CLIENT_APP_KEY = paymentez_client_app_key;
@@ -172,12 +219,15 @@ Paymentez.setEnvironment = function (test_mode, paymentez_client_app_code, payme
  * @param paymentez_client_app_code provided by Paymentez.
  * @param paymentez_client_app_key provided by Paymentez.
  */
-Paymentez.init = function (env_mode, paymentez_client_app_code, paymentez_client_app_key) {
+Paymentez.init = function(
+  env_mode,
+  paymentez_client_app_code,
+  paymentez_client_app_key
+) {
   Paymentez.ENV_MODE = env_mode;
   Paymentez.PAYMENTEZ_CLIENT_APP_CODE = paymentez_client_app_code;
   Paymentez.PAYMENTEZ_CLIENT_APP_KEY = paymentez_client_app_key;
 };
-
 
 /**
  * The simplest way to create a token, using a Card
@@ -188,40 +238,56 @@ Paymentez.init = function (env_mode, paymentez_client_app_code, paymentez_client
  * @param success_callback a callback to receive the token
  * @param failure_callback a callback to receive an error
  */
-Paymentez.addCard = function (uid, email, card, success_callback, failure_callback) {
+Paymentez.addCard = function(
+  uid,
+  email,
+  card,
+  success_callback,
+  failure_callback
+) {
   let session_id = Paymentez.getSessionId();
   Paymentez.dataCollector(session_id);
   let params = {
-    "session_id": session_id,
-    "user": {
-      "id": uid,
-      "email": email,
-      "fiscal_number": $('.fiscal-number').val()
+    session_id: session_id,
+    user: {
+      id: uid,
+      email: email,
+      fiscal_number: $(".fiscal-number").val()
     }
   };
-  params['card'] = card['card'];
+  params["card"] = card["card"];
   Paymentez.createToken(params, success_callback, failure_callback);
 };
 
-
-Paymentez.isCheckout = function () {
+Paymentez.isCheckout = function() {
   return this.ENV_MODE === "";
 };
 
-
-Paymentez.getBinInformation = function (number_bin, form, successCallback, erroCallback) {
+Paymentez.getBinInformation = function(
+  number_bin,
+  form,
+  successCallback,
+  erroCallback
+) {
   let xmlhttp = new XMLHttpRequest();
   if (this.isCheckout()) {
-    let reference = $('#reference').val();
-    let url_bin = "/v2/card_bin/intra/" + number_bin + "/?reference=" + reference;
+    let reference = $("#reference").val();
+    let url_bin =
+      "/v2/card_bin/intra/" + number_bin + "/?reference=" + reference;
     xmlhttp.open("GET", url_bin, true);
   } else {
     let SERVER_URL = this.getServerURL();
     let url_bin = SERVER_URL + "/v2/card_bin/" + number_bin;
     xmlhttp.open("GET", url_bin, true);
-    xmlhttp.setRequestHeader("Auth-Token", this.getAuthToken(this.PAYMENTEZ_CLIENT_APP_CODE, this.PAYMENTEZ_CLIENT_APP_KEY));
+    xmlhttp.setRequestHeader(
+      "Auth-Token",
+      this.getAuthToken(
+        this.PAYMENTEZ_CLIENT_APP_CODE,
+        this.PAYMENTEZ_CLIENT_APP_KEY
+      )
+    );
   }
-  xmlhttp.onreadystatechange = function () {
+  xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState === XMLHttpRequest.DONE) {
       try {
         let objResponse = JSON.parse(xmlhttp.responseText);
@@ -234,10 +300,10 @@ Paymentez.getBinInformation = function (number_bin, form, successCallback, erroC
         }
       } catch (e) {
         let server_error = {
-          "error": {
-            "type": "Server Error",
-            "help": "Server Error",
-            "description": "Server Error"
+          error: {
+            type: "Server Error",
+            help: "Server Error",
+            description: "Server Error"
           }
         };
         erroCallback(server_error);
